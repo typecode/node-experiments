9e112b3794472cae0bbbd53932ad385b751b1c2a
andrew@typeslashcode.com <andrew@typeslashcode.com>
2010-09-03 11:43:48 -0400

    working on dashboard, a small app that checks gmail inbox feeds and pumps to the screen. right now it only outputs to the stdout. also, un/pw are hardcoded, so mine have been removed. soon, they can be stored in a config file, or entered i init, or something else - suggestions welcome

---
+++ dashboard/app.js
@@ -0,0 +1,91 @@
+var http = require('http'),
+    sys = require('sys'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs'),
+    Router = require('./approot/libs/biggie-router'),
+    string = require('./approot/libs/string'),
+    sax = require('./approot/libs/sax'),
+    xml = require('./approot/libs/xml2js'),
+    gmail = require('./approot/gmail');
+
+var app = {
+  name:'dashboard',
+  version:0.1,
+  fetchers:[],
+  conf:[
+    {un:'',pw:'',freq:30000}
+  ]
+}
+
+app.initialize = function(){
+  for(var i in this.conf){
+    var _fetcher = new gmail.fetcher;
+    _fetcher.initialize(app.conf[i]);
+    _fetcher.events.on('gmailReceived',this.gmailReceived);
+    this.fetchers.push(_fetcher);
+  }
+  
+  this.router = new Router();
+  this.router.listen(8123);
+  this.setup_routes();
+}
+
+app.setup_routes = function(){
+  this.router.get('/').get('/index.html').bind(app.getIndex);
+  this.router.get('/inboxes').bind(app.getUpdates);
+  this.router.bind(app.unhandledRequest);
+}
+
+app.getIndex = function(req,res,next){
+  var uri = url.parse(req.url).pathname;
+  if(uri == "/"){ uri = 'index.html'; }
+  var filename = path.join(process.cwd()+'/webroot/', uri);  
+  path.exists(filename, function(exists) {  
+    if(!exists) {
+        res.writeHead(404, {"Content-Type": "text/plain"});  
+        res.write("404 Not Found\n");  
+        res.end();  
+        return;  
+    }
+      
+    fs.readFile(filename, "binary", function(err, file) {  
+      if(err) {  
+        res.writeHead(500, {"Content-Type": "text/plain"});  
+        res.write(err + "\n");  
+        res.end();  
+        return;  
+      }  
+
+      res.writeHead(200);  
+      res.write(file, "binary");  
+      res.end();  
+    });  
+  });
+}
+
+app.getUpdates = function(req,res,next){
+  res.writeHead(200,{'Content-Type':'application/json'});
+  res.end(JSON.stringify({'message':'UPDATES'}));
+}
+
+app.unhandledRequest = function(req,res,next){
+  res.writeHead(404,{'Content-Type':'application/json'});
+  res.end(JSON.stringify({'message':'Resource Not Found'}));
+}
+
+app.gmailReceived = function(data){
+  var parser = new xml.Parser();
+  parser.addListener('end', function(result) {
+    var output = result.title+" ->";
+    output = output.rpad(' ',55) + result.fullcount + ' |';
+    for(var i = 0; i < result.fullcount; i++){
+      output = output + "#";
+    }
+    console.log(output);
+  });
+  parser.parseString(data);
+}
+
+app.initialize();
+
